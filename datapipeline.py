5b85bcf85a397a1198662fe086a212a4eb1a1849
Optimus-Q <pereira.melroy@outlook.com>
2025-12-06 22:15:23 +1030

    Adding local files

---
+++ datapipeline.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+
+def DataMapper():
+    with open('./data/satelite mapper/satelitecode_mapper.json', 'r') as f:
+        map_data = json.load(f)
+    return map_data
+
+def DataPipeline(data):
+    data_ = data.dropna()
+    map_data_satelite_code = DataMapper()
+    data_['Satelite_Code'] = data_['Satelite_Code'].map(map_data_satelite_code)
+    return data_
